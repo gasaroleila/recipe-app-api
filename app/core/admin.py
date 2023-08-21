d79eb801ba67aa50efc0fe977632a04971bc330d
gasaroleila <uwamgaleila@gmail.com>
2023-08-21 08:53:10 +0200

    added api documentation

--- app/core/admin.py
+++ app/core/admin.py
@@ -15,17 +15,33 @@ class UserAdmin(BaseUserAdmin):
     list_display = ['email', 'name']
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
+        (_('Personal Info'), {'fields': ('name',)}),
         (
-           _('Permissions'),
-           {
-               'fields': (
-                   'is_active',
-                   'is_staff',
-                   'is_superuser'
-               )
-           }
+        _('Permissions'),
+        {
+            'fields': (
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            )
+            }
         ),
-        (_('Important dates', {'fields': ('last_login')}))
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
+    add_fieldsets = (
+        (
+            None, {
+                'classes': ('wide'),
+                'fields': (
+                    'email',
+                    'password1',
+                    'password2',
+                    'name',
+                    'is_active',
+                    'is_staff',
+                    'is_superuser'
+                )
+            }),
     )
 
     readonly_fields = ['last_login']
